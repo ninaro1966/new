35918e91d3b8ce112ba0d2636f9cd3c43c356122
ninaro1966 <ovchinnikova@specmont.ru>
2022-08-12 22:22:27 +0300

    init commit

---
+++ october/Program.cs
@@ -0,0 +1,19 @@
+﻿//1/////
+// Написать программу, которая из имеющегося массива строк
+// формирует массив из строк, длина которых меньше или равно 3 символам;
+string[]array1 = {"hello", "2", "world", ":-)"};
+string[]array2 = new string[array1.Length];
+int j = 0;  
+ Console. Write($"[");
+ for ( int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3) 
+        
+            { 
+            array2[j] = array1[i]; 
+           
+          Console. Write(array2[j] + " ");
+           j++;
+           }
+    }
+Console. Write($"]");
